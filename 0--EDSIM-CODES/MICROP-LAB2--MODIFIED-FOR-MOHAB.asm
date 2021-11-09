3912c703232a74d74a88ceb3e5eafea3ba5f83e1
Fengchu ZHANG <davidfczhang@outlook.com>
2021-11-09 13:30:50 +0800

    Finished work, v1.0

--- 0--EDSIM-CODES/MICROP-LAB2--MODIFIED-FOR-MOHAB.asm
+++ 0--EDSIM-CODES/MICROP-LAB2--MODIFIED-FOR-MOHAB.asm
@@ -1,3 +1,4 @@
+; Name: Mohab Mohamed Maher  
 ; Course code: EEE3431
 ; Experiment/Assignment No: Experiment #1 Basic IO and Arithmetic
 ; Emulator version: 2.1.20  
@@ -10,12 +11,12 @@
 ; 4. DISP-1: The 2nd 7-segment display from the right. This one is used for the ten's digit of the countdown.
 ; 5. 0B3H: The bit address for P3.3.
 ; # Notes: 
-; Ignore the 7-segment display (DISP-1) while LIGHTS is executing. 
-; Ignore LEDs when 7-segment display (DISP-0) is executing COUNTDOWN_SUB.
+; Ignore the 7-segment display (DISP-2) while LIGHTS is executing. 
+; Ignore LEDs when 7-segment display (DISP-0 and DISP-1) is executing COUNTDOWN_SUB.
 
 START:
-SETB P3.3 ;| Initializing to choose DISP-1 (the ignored one).
-CLR P3.4 ;|
+CLR P3.3 ;| Initializing to choose DISP-2 (the ignored one).
+SETB P3.4 ;|
 LIGHTS:
 MOV P1, #11110110B ; #1 Initialize LEDs as both RED.
 CALL DELAY_LONG ; #2
@@ -33,10 +34,46 @@ MOV P1, #11110101B ; #17 Change X from green to yellow.
 CALL DELAY_LONG ; #18
 JMP LIGHTS ; Retvrn.
 
+;---
+
 COUNTDOWN_SUB:  
 ; Ignore lights while 7-seg countdown.
 MOV P1, #11111111B ; Reset the 7-segment display to all-off.  
-CPL 0B3H ; #5, 14 Switch to DISP-0 (the countdown display).
+
+; Count down from 12 to 10.
+SETB P3.3
+CLR P3.4 
+MOV P1, #11111001B ; Put 1 on DISP-1.
+MOV P1, #11111111B ; Reset the 7-segment display to all-off. Prevent shifting display between DISP-1 and DISP-2.  
+CLR P3.3
+CLR P3.4
+MOV P1, #10100100B ; Put 2 on DISP-0. This makes "12". 
+;CALL DELAY_1S
+MOV P1, #11111111B ; Reset the 7-segment display to all-off. Prevent shifting display between DISP-1 and DISP-2.  
+
+SETB P3.3
+CLR P3.4 
+MOV P1, #11111001B ; Put 1 on DISP-1.
+MOV P1, #11111111B ; Reset the 7-segment display to all-off. Prevent shifting display between DISP-1 and DISP-2.  
+CLR P3.3
+CLR P3.4
+MOV P1, #11111001B ; Put 1 on DISP-1. This makes "11".
+;CALL DELAY_1S
+MOV P1, #11111111B ; Reset the 7-segment display to all-off. Prevent shifting display between DISP-1 and DISP-2.  
+
+SETB P3.3
+CLR P3.4 
+MOV P1, #11111001B ; Put 1 on DISP-1.
+MOV P1, #11111111B ; Reset the 7-segment display to all-off. Prevent shifting display between DISP-1 and DISP-2.  
+CLR P3.3
+CLR P3.4
+MOV P1, #11000000B ; Put 0 on DISP-0. This makes "10". 
+CALL DELAY_1S
+
+
+; Countdown from 9 t0 0. 
+CLR P3.3 ; #5, 14 Switch to DISP-0 only.
+CLR P3.4
 MOV P1, #10010000B ; Put 9 on DISP-0.
 CALL DELAY_SHORT
 MOV P1, #10000000B ; Put 8 on DISP-0.
@@ -57,7 +94,15 @@ MOV P1, #11111001B ; Put 1 on DISP-0.
 CALL DELAY_SHORT
 MOV P1, #11000000B ; Put 0 on DISP-0.
 CALL DELAY_SHORT
-CPL 0B3H ; #7, 16 Switch to DISP-1 (the ignored one).
+CLR P3.3 ;| Choose DISP-2 (the ignored one).
+SETB P3.4 ;|
+RET
+
+;---
+
+DELAY_1S: 
+MOV R0, #1H
+DJNZ R0, $
 RET
 
 DELAY_SHORT:  
